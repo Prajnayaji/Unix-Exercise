8e864abd5e61623300289aa1ec6d1822bc466eb6
Prajnayaji <35536603+Prajnayaji@users.noreply.github.com>
2020-01-28 14:59:39 +0530

    Update ls.c

--- que3/ls.c
+++ que3/ls.c
@@ -9,13 +9,21 @@ int main()
 	char path[100];
 	DIR *directory;
 	struct dirent *d;
-
+	FILE *f;
 	printf("Enter the absolute path of the file\n");
 	scanf("%[^\n]",path);
 	printf("File path is: %s\n",path);
 	directory = opendir(path);
 	if ( directory == NULL)
 	{
+		f=fopen(path,"r");
+		if ( f != NULL)
+		{
+			printf("It is a file\n");
+			return -1;
+
+		}
+
 		printf("No such file or directory\n");
 		return -1;
 	}
